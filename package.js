2cbc445c55208a11e1bd1440c0c7584a9646ea41
Nathan Strauser <nathan.strauser@gmail.com>
2014-08-27 20:44:27 -0400

    0.9.0 update

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-    summary: "Select2 is a jQuery based replacement for select boxes."
+    name:"natestrauser:select2",
+    summary: "Select2 is a jQuery based replacement for select boxes.",
+  version: "3.4.6",
+  git: "https://github.com/nate-strauser/meteor-select2.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
     api.use("jquery", "client");
 
     api.add_files([
