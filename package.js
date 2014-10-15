b5b6f6abc4e5fe4df1cc8b3c889fac64d8ba7b4d
Nathan Strauser <nathan.strauser@gmail.com>
2014-10-15 18:13:08 -0400

    #4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name:"natestrauser:select2",
     summary: "Select2 is a jQuery based replacement for select boxes.",
-  version: "3.4.9",
+  version: "3.5.1",
   git: "https://github.com/nate-strauser/meteor-select2.git"
 });
 
